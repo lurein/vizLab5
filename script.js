11be367d8d39155e4ee891f89c8aa889d4501b1e
Lurein Perera <pererag@bc.edu>
2020-10-22 00:01:58 -0400

    last submission

--- script.js
+++ script.js
@@ -1,80 +1,7 @@
-// d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
-//     console.log('data', data);
-//     const margin = ({top: 20, right: 20, bottom: 20, left: 60});
-//     const width = 700 - margin.left - margin.right;
-//     const height = 550 - margin.top - margin.bottom;
-//     const svg = d3.select('.chart')
-//         .append('svg')
-//         .attr('width', width + margin.left + margin.right)
-//         .attr('height', height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     let companiesArray = [];
-//     let storesArray = [];
-
-//     for (let step = 0; step < 8; step++) { // in this block we're creating new arrays with every income value and every life expectancy val
-//         companiesArray.push(data[step].company);
-//         storesArray.push(data[step].stores);
-//     };
-
-//     console.log(companiesArray)
-//     console.log(storesArray)
-//     console.log(d3.extent(storesArray))
-//     const xScale = d3
-//         .scaleBand()
-//         .domain(companiesArray)
-//         .rangeRound([0, width])
-//         .paddingInner(0.1)
-
-//     const yScale = d3
-//         .scaleLinear()
-//         .domain([0, 21366])
-//         .range([height, 0])
-
-//     console.log(yScale(21366))
-
-//     svg.append('g')
-//         .selectAll("bars")
-//         .data(data)
-//         .enter()
-//         .append('rect')
-//             .attr("class", "bar")
-//             .attr('x', d=>xScale(d.company) )
-//             .attr('y', d=>yScale(d.stores))
-//             .attr('width', xScale.bandwidth())
-//             .attr('height', d=>height - yScale(d.stores))
-//             .style("fill", 'steelblue');
-    
-//     const xAxis = d3.axisBottom()
-//         .scale(xScale)
-    
-//     // drawing the x-axis
-//     svg.append("g")
-//         .attr("class", "axis x-axis")
-//         .attr("transform", `translate(0, ${height})`)
-//         .call(xAxis);
-
-//     const yAxis = d3.axisLeft()
-//         .scale(yScale)
-
-//     svg.append("g")
-//         .attr("class", "axis y-axis")
-//         .attr("transform", `translate(0, 0)`)
-//         .call(yAxis);
-
-    
-//     svg.append("text")
-//         .attr('x', 0)
-//         .attr('y', 0)
-//         .text("Stores");
-
-// })
-
 
 // CHART INIT ------------------------------**
 let type = d3.select('select').node().value
-console.log(type)
+let prevY = 0
 
 // create svg with margin convention
 const margin = ({top: 20, right: 20, bottom: 20, left: 60});
@@ -130,14 +57,18 @@ function update(data, type){
     xScale.domain(data.map(function(d) { return d.company; }));
     yScale.domain([0, d3.max(data, function(d) { return d[type]; })]);
     // update bars
+    svg.selectAll(".bar").remove()
+
     const bars = svg.append('g')
-        .selectAll("bars")
+        .selectAll(".bar")
         .data(data)
         .enter()
         .append('rect')
             .attr("class", "bar")
             .attr('x', d=>xScale(d.company) )
-            .attr('y', d=>yScale(d[type]))
+            .attr('y', function(d) { return yScale(d[type]) } )
+            .transition()
+            .duration(1000)
             .attr('width', xScale.bandwidth())
             .attr('height', d=>height - yScale(d[type]))
             .style("fill", 'steelblue');
@@ -167,6 +98,8 @@ function selectorChanged() {
     d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
         update(data, type); // simply call the update function with the supplied data**
     });
+
+    d3.selectAll(".bar")
 }
 
 // (Later) Handling the sorting direction change
